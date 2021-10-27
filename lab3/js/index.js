5b5472523928a61e9c498642205e10b0d916473b
YuriiVavrynchuk <yvavrynchuk@tsukat.com>
2021-10-27 22:36:36 +0300

    added files for lab 4-5

--- lab3/js/index.js
+++ lab3/js/index.js
@@ -1,5 +1,20 @@
-import { renderItemsList } from "./dom_util.js";
-import { getAllPrinters } from "./printers.js";
+import { 
+    getInputValues,
+    renderItemsList,
+    EDIT_BUTTON_PREFIX,
+    DELETE_BUTTON_PREFIX,
+    clearInputs
+} from "./dom_util.js";
+
+import { 
+    getAllPrinters, 
+    postPrinter, 
+    updatePrinter,
+    deletePrinter
+} from "./api.js";
+
+const formField = document.getElementById("item_form");
+const submitButton = document.getElementById("submit_button");
 
 const searchButton = document.getElementById("search__button");
 const clearSearchButton = document.getElementById("clear__search__button");
@@ -9,22 +24,58 @@ const countButton = document.getElementById("count__button");
 
 let printers = [];
 
-export const refetchAllPrinters = () => {
-    const allPrinters = getAllPrinters();
+const onEditItem = async (e) => {
+    const itemId = e.target.id.replace(EDIT_BUTTON_PREFIX, "");
+
+    await updatePrinter(itemId, getInputValues());
+
+    clearInputs();
+
+    refetchAllPrinters();
+};
+
+const onRemoveItem = async (element) => {
+    const itemId = element.target.id.replace(DELETE_BUTTON_PREFIX, "");
+
+    await deletePrinter(itemId);
+
+    refetchAllPrinters(); 
+}
+
+export const refetchAllPrinters = async () => {
+    const allPrinters = await getAllPrinters();
 
     printers = allPrinters.sort((a, b) => b.model.localeCompare(a.model));
 
-    renderItemsList(printers);
+    renderItemsList(printers, onEditItem, onRemoveItem);
 };
 
-searchButton.addEventListener("click", () => {
-    const foundPrinters = printers.filter((printer) => printer.model.search(searchInput.value) !== -1);
+submitButton.addEventListener("click", async (event) => {
+    event.preventDefault();
 
-    renderItemsList(foundPrinters);
+    const { brand, model, power, price } = getInputValues();
+
+    clearInputs();
+
+    postPrinter({
+        brand,
+        model, 
+        power,
+        price,
+    }).then(refetchAllPrinters);
+
+});
+
+searchButton.addEventListener("click", () => {
+    const foundPrinters = printers.filter(
+        printer => printer.brand.search(searchInput.value) !== -1
+    );
+
+    renderItemsList(foundPrinters, onEditItem, onRemoveItem);
 });
 
 clearSearchButton.addEventListener('click', () => {
-    renderItemsList(printers);
+    renderItemsList(printers, onEditItem, onRemoveItem);
 
     searchInput.value = "";
 });
@@ -34,7 +85,7 @@ sortCheckbox.addEventListener("change", function() {
         const sortedPrinters = printers.sort(
             (a, b) => parseInt(a.price) - parseInt(b.price));
         
-        renderItemsList(sortedPrinters);
+        renderItemsList(sortedPrinters, onEditItem, onRemoveItem);
     } else {
         refetchAllPrinters();
     }
