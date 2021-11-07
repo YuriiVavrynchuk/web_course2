ca955ab877df764bc973f9db1ae502076e3c3cca
YuriiVavrynchuk <yvavrynchuk@tsukat.com>
2021-11-07 21:43:29 +0200

    added files for lab 6-7

---
+++ lab6/src/containers/Home/Home.styled.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const SectionWrapper = styled.div`
+    display: flex;
+    background: #FFFFFF;
+    width:100%;
+    justify-content: space-around;
+    padding: 0 auto;
+    img {
+        width : 400px;
+        margin-left: 10%;
+    }
+`;
+
+export const StyledText = styled.div`
+    padding-top: 70px;
+    h1 {
+        font-size: 40px;
+        margin-right: 20%;
+        color: red;
+    }
+`
+
+export const CardWrapper = styled.div`
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    margin-top: 60px;
+`
